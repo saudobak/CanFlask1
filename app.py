f2fa68ee5e50ac6b9756683e85feb334d93ad5a7
saudobak <aurelia.smith@cv.edu>
2024-02-06 21:03:41 +0000

    Added second route

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def hello():
     return "Hello Mundo!"
 
+@ap.route("/about")
+def hello_about(about):
+    return "Hola " + name
+
 if __name__ == "__main__":
     app.run(debug=True)
     
